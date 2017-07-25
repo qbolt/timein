c0009be5f239a16d96f04a0e9358b339914840bd
Quinton <bolt.quinton@yahoo.com>
2017-07-24 20:14:11 -0500

    changed request to use google, added proxy option

--- src/timein.js
+++ src/timein.js
@@ -1,21 +1,41 @@
-const got = require('got')
+#!/usr/bin/env node
+const request = require('request')
 const meow = require('meow')
 
-const makeRequest = (url) => {
-    got(url).then((result) => {
-        const $ = result.body
-        const location = $.split('<tr><td><a href=')[1].split('</a>')[0].split('>')[1]
-        console.log(location)
-    const answer = $.split('class=rbi>')[1].split('</td>')[0]
-    console.log(answer)
-    }).
-        catch(err => {
-            console.log('Error: No results found.')
+const makeRequest = (options) => {
+    request(options, (err, status, result) => {
+        if (err) {
+            console.log('No results found.')
+        } else {
+            const $ = result.split('<div class="_rkc _Peb">')[1].split('</div></div></div>')[0]
+            const location = $.split('<span class="_HOb _Qeb">   Time in ')[1].split(' </span>')[0]
+            const time = $.split('</div>')[0]
+            let date = $.split('<div class="_HOb _Qeb"> ')[1].split('</div>')[0]
+            date = date.replace('<span style="white-space:nowrap">', '').replace('</span>', '')
+            console.log(`${location}\n${time} ${date}`)
+        }
     })
 }
 
-const cli = meow()
-const url = 'http://www.timeanddate.com/worldclock/?query='
-const query = cli.input[0] ? cli.input[0] : ''
+const cli = meow(`
+    Usage
+        $ timein <input>
 
-makeRequest(url + query)
+    Options
+        -p, --proxy             Set your proxy
+        -h, --help              Display helpful information
+`, {
+    alias: {
+        p: '--proxy',
+        h: '--help'
+    }
+})
+
+const url = 'http://www.google.com/search?q=time+in+' + (cli.input[0] ? cli.input.join('+') : '')
+const options = Object.assign(
+    {},
+    { url },
+    cli.flags.p && { proxy: cli.flags.p }
+)
+
+makeRequest(options)
